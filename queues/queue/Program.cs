7fdb2a1448d945b0d34b000bad3ab92e43ab6a37
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-09-18 15:58:04 +0500

    implemented basic queue

---
+++ queues/queue/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+    CustomBasicQueue<int> st = new CustomBasicQueue<int>(3);
+
+    st.Enqueue(22);
+    st.Show();
+    st.Enqueue(33);
+    st.Enqueue(43);
+    Console.WriteLine(st.GetSize());
+    // st.Show();
+    // st.DeQueue();
+    // st.Show();
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadLine();
+
+
+
